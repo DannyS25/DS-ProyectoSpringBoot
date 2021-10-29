e5017b97e5b20369549262ac6ff7798ee54b03d3
Danny Steven Rodriguez Hurtado <drodriguezh@git.local>
2021-10-29 18:07:16 -0500

    Nuevo proyecto Spring Boot v0.1

---
+++ Backend/src/main/java/com/example/PruebaSpringClase8/PruebaSpringClase8Application.java
@@ -0,0 +1,13 @@
+package com.example.PruebaSpringClase8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringClase8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringClase8Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/PruebaSpringClase8/PruebaSpringClase8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PruebaSpringClase8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringClase8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
